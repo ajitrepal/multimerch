c054122f2b18733b20c8acbee4d995dcdc3afdc5
Ajit Repal <55535196+ajitrepal@users.noreply.github.com>
2023-03-21 11:18:14 +0530

    Create Testradio.php

---
+++ Testradio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Player</title>
+</head>
+<body>
+	<div>
+		<video width="100" height="100" controls>
+			<source src="http://stream-51.zeno.fm/u0hrd3xkzhhvv?zs=L7rSq-7VTbOF0o3pj-tXzg" type="application/x-mpegURL">
+			Your browser does not support HTML5 video.
+		</video>
+	</div>
+</body>
+</html>
